8476b3ce7da0f18f955b1d65fb87c876242003a0
J <zyjj217@gmail.com>
2022-10-30 22:35:12 +0800

    finished driver funcs for now

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, session
 import pymysql.cursors
-import hashlib
-import loginSession
-
 
 
 app = Flask(__name__)
@@ -46,13 +43,14 @@ def userAuth():
 
     if(data):
         session['username'] = username
+        session['userID'] = data['userID']
+        session['mode'] = 0
         return 'true'
     else:
         error = 'Credentials Wrong'
         return 'false'
 
 
-#Runiing backendd tests , building user session
 @app.route("/gate/driverAuth", methods = ['POST', 'GET'])
 def driverAuth():
     username = request.json.get('username')
@@ -67,11 +65,12 @@ def driverAuth():
     cursor.close()
     error = None
 
-#running tests here
+#once logged in store sessiondata
     if(data):
         session['username'] = username
         session['driverID'] = data['driverID']
         session['mode'] = "1"
+        session['myOrders'] = {}
         return 'true'
     else:
         error = 'Credentials Wrong'
@@ -96,6 +95,8 @@ def shopAuth():
 
     if(data):
         session['username'] = username
+        session['shopID'] = data['shopID']
+        session['mode'] = 2
         return 'true'
     else:
         error = 'Credentials Wrong'
@@ -115,18 +116,16 @@ def driverReg():
 def shopReg():
     return "shopReg"
 
-# cursor = conn.cursor()
-# query = 'SELECT * from users where userID = 0'
-# cursor.execute(query)
-# data = cursor.fetchall()
+
 
 #user actions
     #get orderlist
-@app.route("/user/getOrderlist", methods = ('POST', 'GET'))
-def getOrderlist():
-    cursor = conn.cursor()
-    query = 'SELECT * from orders WHERE userID = %s'
-    cursor.execute(query, userID)
+# @app.route("/user/getOrderlist", methods = ('POST', 'GET'))
+# def getOrderlist():
+#
+#     cursor = conn.cursor()
+#     query = 'SELECT * from orders WHERE userID = %s'
+#     cursor.execute(query, userID)
 
     #get shoplist
     #get itemslist from shops
@@ -137,10 +136,51 @@ def getOrderlist():
 
 
 #driver actions
-    #get orderlist
-    #accept order
-    #finish order
+#get orderlist
+@app.route("/driver/getOrderlist", methods = ["POST", 'GET'])
+def driverGetOrderlist():
+    driverID = session['driverID']
+    cursor = conn.cursor()
+    query = 'SELECT * from orders WHERE driverID = %s'
+    cursor.execute(query, driverID)
+    data = cursor.fetchall()
+    return data
 
+    #accept order
+
+#experimental
+@app.route("/driver/acceptOrder", methods = ["POST", "GET"])
+def driverAcceptOrder():
+
+    orderID = request.json.get('orderID')
+    status = 'Accepted'
+    driverID = session['driverID']
+
+
+    cursor = conn.cursor()
+    query = 'UPDATE orders SET status = %s, driverID = %s WHERE orders.orderID = %s'
+    cursor.execute(query, (status, driverID, orderID))
+    conn.commit()
+    cursor.close()
+
+    return "Success"
+
+
+#finish order
+@app.route("/driver/finishOrder", methods = ["POST", "GET"])
+def driverFinishOrder():
+    orderID = request.json.get('orderID')
+    status = 'Delivered'
+    driverID = session['driverID']
+
+
+    cursor = conn.cursor()
+    query = 'UPDATE orders SET status = %s, driverID = %s WHERE orders.orderID = %s'
+    cursor.execute(query, (status, driverID, orderID))
+    conn.commit()
+    cursor.close()
+
+    return "Success"
 
 
 
@@ -151,6 +191,17 @@ def getOrderlist():
 #delete item
 #get orderlist
 #accept order change status
+
+#global methods
+@app.route('/global/getOrders', methods = ['POST','GET'])
+def getOrders():
+    cursor = conn.cursor()
+    query = 'SELECT * from orders'
+    cursor.execute(query)
+    data = cursor.fetchall()
+    return data
+
+
 
 
 app.secret_key = 'some key that you will never guess'
